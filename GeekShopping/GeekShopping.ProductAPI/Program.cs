bbf18645e0877f69de7fce030d5b4ab0282b7761
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-08-05 00:21:53 -0300

    Iniciando e Finalizando a Implementação do Repositorio de Produtos.

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +13,10 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
 	connection,
 	new MySqlServerVersion(new Version(8, 0, 29)))
 );
+
+IMapper mapper = MapperConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
