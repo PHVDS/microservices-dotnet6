90abdf81ed2dbfebecba25eadd57a2d1b0eedc73
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2023-05-23 19:12:07 -0300

    Setando o Jwt Token no Header(Cabeçalho) das Operações.

--- GeekShopping/GeekShopping.Web/Services/ProductService.cs
+++ GeekShopping/GeekShopping.Web/Services/ProductService.cs
@@ -2,6 +2,7 @@
 using GeekShopping.Web.Services.IServices;
 using GeekShopping.Web.Utils;
 using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace GeekShopping.Web.Services
 {
@@ -17,37 +18,42 @@ namespace GeekShopping.Web.Services
 			_client = client ?? throw new ArgumentNullException(nameof(client));
 		}
 
-		public async Task<IEnumerable<ProductModel>> FindAllProducts()
+		public async Task<IEnumerable<ProductModel>> FindAllProducts(string token)
 		{
+			_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 			var response = await _client.GetAsync(BasePath);
 			return await response.ReadContentAs<List<ProductModel>>();
 		}
 
-		public async Task<ProductModel> FindProductById(long id)
+		public async Task<ProductModel> FindProductById(long id, string token)
 		{
-			var response = await _client.GetAsync($"{BasePath}/{id}");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.GetAsync($"{BasePath}/{id}");
 			return await response.ReadContentAs<ProductModel>();
 		}
 
-		public async Task<ProductModel> CreateProduct(ProductModel model)
+		public async Task<ProductModel> CreateProduct(ProductModel model, string token)
 		{
-			var response = await _client.PostAsJson(BasePath, model);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.PostAsJson(BasePath, model);
 			if (response.IsSuccessStatusCode)
 				return await response.ReadContentAs<ProductModel>();
 			else throw new Exception("Algo deu errado ao chamar a API");
 		}
 
-		public async Task<bool> DeleteProductById(long id)
+		public async Task<bool> DeleteProductById(long id, string token)
 		{
-			var response = await _client.DeleteAsync($"{BasePath}/{id}");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.DeleteAsync($"{BasePath}/{id}");
 			if (response.IsSuccessStatusCode)
 				return await response.ReadContentAs<bool>();
 			else throw new Exception("Algo deu errado ao chamar a API");
 		}
 
-		public async Task<ProductModel> UpdateProduct(ProductModel model)
+		public async Task<ProductModel> UpdateProduct(ProductModel model, string token)
 		{
-			var response = await _client.PutAsJson(BasePath, model);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.PutAsJson(BasePath, model);
 			if (response.IsSuccessStatusCode)
 				return await response.ReadContentAs<ProductModel>();
 			else throw new Exception("Algo deu errado ao chamar a API");
