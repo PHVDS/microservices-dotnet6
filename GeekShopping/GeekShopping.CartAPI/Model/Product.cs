160113e161e776280bdd9394e54bbeabde2a3938
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2023-05-26 15:48:30 -0300

    Criando os VO's(ValueObjects) e ajustando as configurações do AutoMapper.

--- GeekShopping/GeekShopping.CartAPI/Model/Product.cs
+++ GeekShopping/GeekShopping.CartAPI/Model/Product.cs
@@ -4,33 +4,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GeekShopping.CartAPI.Model
 {
-	[Table("product")]
-	public class Product
-	{
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
-        [Column("id")]
-        public long Id { get; set; }
-
+    [Table("product")]
+    public class Product : BaseEntity
+    {
         [Column("name")]
-		[Required]
-		[StringLength(150)]
-		public string Name { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
 
-		[Column("price")]
-		[Required]
-		[Range(1, 10000)]
-		public decimal Price { get; set; }
-		
-		[Column("description")]
-		[StringLength(500)]
-		public string? Description { get; set; }
+        [Column("price")]
+        [Required]
+        [Range(1, 10000)]
+        public decimal Price { get; set; }
 
-		[Column("category_name")]
-		[StringLength(50)]
-		public string CategoryName { get; set; }
+        [Column("description")]
+        [StringLength(500)]
+        public string Description { get; set; }
 
-		[Column("image_url")]
-		[StringLength(300)]
-		public string? ImageURL { get; set; }
-	}
+        [Column("category_name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string ImageURL { get; set; }
+    }
 }
