12bbdcd8e04083412055e01b18e8eb1bb163d34e
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2023-06-06 16:48:36 -0300

    Criando as ViewModels do carrinho de compras.

---
+++ GeekShopping/GeekShopping.Web/Models/CartViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class CartViewModel
+    {
+        public CartHeaderViewModel CartHeader { get; set; }
+
+        public IEnumerable<CartDetailsViewModel> CartDetails { get; set; }
+    }
+}
