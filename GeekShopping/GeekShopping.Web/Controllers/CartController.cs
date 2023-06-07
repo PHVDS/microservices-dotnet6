c2211f14e2e72f05364b4364bae6f1b6489c757e
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2023-06-07 14:00:10 -0300

    Adicionando o carrinho de compras ao menu e criando o CartController.

---
+++ GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService, IProductService productService)
+        {
+            _cartService = cartService;
+            _productService = productService;
+        }
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+    }
+}
