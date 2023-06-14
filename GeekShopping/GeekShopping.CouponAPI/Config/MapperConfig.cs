43022e0574f6711f620eedb4d050d4bf11cf3efb
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2023-06-14 20:18:50 -0300

    Configurando a classe Program e DbContext do GeekShopping.CouponAPI.

---
+++ GeekShopping/GeekShopping.CouponAPI/Config/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GeekShopping.CouponAPI.Model;
+
+namespace GeekShopping.CouponAPI.Config
+{
+	public class MapperConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			var mappingConfig = new MapperConfiguration(config => {
+				//config.CreateMap<ProductVO, Product>().ReverseMap();
+			});
+			return mappingConfig;
+		}
+	}
+}
